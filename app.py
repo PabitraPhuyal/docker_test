ca9f153f7f24466f601c1c809f637c429369ab9f
Pabitraphuyal <phuyalpabitra12@gmail.com>
2026-04-22 15:55:56 -0500

    added simple code

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Docker is running successfully!")
+def test_basic_math():
+    assert 1 + 1 == 2
+
+def test_string_ops():
+    name = "jenkins"
+    assert name.upper() == "JENKINS"
+
+def test_list_ops():
+    items = [1, 2, 3]
+    assert len(items) == 3
